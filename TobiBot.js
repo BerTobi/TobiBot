bc9871c688f731b45098d4d78410465674f0f5fb
Tobix192 <tobiasbersia@gmail.com>
2018-09-14 20:31:41 -0200

    First version released
    First release of this bot. (Currently for Cookie Clicker Classic)

---
+++ TobiBot.js
@@ -0,0 +1,13 @@
+CookieClicker = {
+	start: function() {
+		this.clickInterval = setInterval(function(){
+			// Click the large cookie as fast as possible!
+			ClickCookie();
+		}, 300);
+
+
+	},
+	stop: function() {
+		clearInterval(this.clickInterval);
+	}
+};
